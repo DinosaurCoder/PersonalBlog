524a03f06bb6defc9ae6c4778653d6f3c3831b80
ZhangTingDong <765752684@qq.com>
2020-01-26 19:40:05 +0800

    master

--- dao/dbUtil.js
+++ dao/dbUtil.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 function createConnection (){
 
     var connection = mysql.createConnection({
-        host: "localhost",
+        host: "192.168.0.104",
         port: "3306",
         user: "root",
         password: "123",
