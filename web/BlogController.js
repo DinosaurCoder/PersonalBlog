68164b7e6c6b48d15a50788c7b6daf724358756a
ZhangTingDong <765752684@qq.com>
2020-02-14 19:15:43 +0800

    master

--- web/BlogController.js
+++ web/BlogController.js
@@ -16,7 +16,6 @@ path.set("/queryHotBlog",queryHotBlog);
 function queryBlogById(request,response){
     var params = url.parse(request.url, true).query;
     blogDao.queryBlogId(parseInt(params.bid),function(result){
-        console.log(result);
         response.writeHead(200);
         response.write(respUtil.writeResult("success", "查询成功", result));
         response.end();
@@ -47,11 +46,8 @@ function queryBlogByPage(request, response) {
     var params = url.parse(request.url, true).query;
     blogDao.queryBlogByPage(parseInt(params.page), parseInt(params.pageSize), function (result) {
         for(var i=0; i<result.length;i++){
-            result[i].content = result[i].content.replace(/<\/?.+?\/?>/g,"");
-            result[i].content = result[i].content.replace(/&nbsp;/g,"");
-            result[i].content = result[i].content.replace(/#;/g,"");
-            result[i].content = result[i].content.replace(/<[\w\W]{1,5}>/g,"");
-            result[i].content = result[i].content.substring(0,300);
+            result[i].content = result[i].content.replace(/<[^>]+>/g," ");
+            result[i].content = result[i].content.substring(0,400);
         }
         response.writeHead(200);
         response.write(respUtil.writeResult("success", "查询成功", result));
@@ -61,14 +57,12 @@ function queryBlogByPage(request, response) {
 path.set("/queryBlogByPage", queryBlogByPage);
 
 function editBlog(request, response) {
-
     var params = url.parse(request.url, true).query;
     var tags = params.tags.replace(/ /g, "").replace("，", ",");
     request.on("data", function (data) {
-        console.log(data.toString());
         blogDao.insertBlog(params.title, data.toString(), 0, tags, timeUtil.getNow(), timeUtil.getNow(), function (result) {
             try{
-                response.writeHead(200);
+                response.writeHead(200,{'Content-Type':'text/html;charset=UTF8'});
                 response.write(respUtil.writeResult("success", "添加成功", null));
                 response.end();
             }catch(err){
